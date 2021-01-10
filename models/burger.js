1f54c78005794ed6baa2bdf14125f1217d4b5907
Kari Smith <evanjalioto@gmail.com>
2021-01-09 20:45:32 -0800

    Setup Burger.js model file

---
+++ models/burger.js
@@ -0,0 +1,50 @@
+const connection = require("../config/connection");
+const orm = require("../config/orm");
+
+var burgers = {
+    getBurgers: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  insertOne: function(table, cols, vals, cb) {
+      var queryString = "INSERT INTO " + table;
+
+      queryString += " (";
+      queryString += cols.toString();
+      queryString += ") ";
+      queryString += "VALUES (";
+      queryString += printQuestionMarks(vals.length);
+      queryString += ") ";
+
+      console.log(queryString);
+
+      connection.query(queryString, vals, function(err, res) {
+          if (err) {throw err;}
+            cb(res);
+      });
+  },
+  updateOne: function(table, objColVals, conditionq, cb) {
+      var queryString = "UPDATE " + table;
+
+      queryString += " SET ";
+      queryString += objToString(objColVals);
+      queryString += " Where ";
+      queryString += conditionq;
+
+      console.log(queryString);
+      connection.query(queryString, function(err, res) {
+          if(err) {
+              throw err;
+          }
+          cb(res);
+      });
+  }
+};
+
+// Export functions for controller (burgers)
+module.exports = burger;
