038f86a2965c7ea4f7894b8238004c2bf26c1e2a
Kari Smith <evanjalioto@gmail.com>
2021-01-10 20:42:24 -0800

    Set up server.js required express, handlebarss and imported routes so server can access

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+// Static content for the app from the "public" directory
+app.use(express.static("public"));
+
+// parse app body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burger_time_controller");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client
+app.listen(PORT, function() {
+    // Log (server-side) When our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+});
