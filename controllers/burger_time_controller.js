c5ce93f326892cdb9b5ec568a479143bc716b8a3
Kari Smith <evanjalioto@gmail.com>
2021-01-10 20:02:03 -0800

    added get post and put functions on controller

--- controllers/burger_time_controller.js
+++ controllers/burger_time_controller.js
@@ -1,5 +1,46 @@
 const express = require ("express");
 const burger = require ('../models/burger');
 
+var router = express.Router();
+
 // Routing Functionality
+
+router.get("/", function(req, res) {
+    cat.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+
+router.post("api/burgers", function(req, res) {
+    burger.create{[
+        "burger_name", "devoured"
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function(result) {
+        res.json({ id: result.insertId});
+    }};
+});
+
+router.put("/api/cats/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    cat.update({
+        devoured: req.body.devoured
+    }, condition, function(result) {
+        if(result.changedRows == 0) {
+            return res.status(404).end();
+    
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+
 
